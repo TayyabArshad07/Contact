6e5f60e9135c2372795403c396d20476ec77e837
TayyabArshad07 <150454391+TayyabArshad07@users.noreply.github.com>
2024-02-10 07:25:37 -0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ContactUs from '@/pages/Components/C/ContactUs'
+import Chatbot from '@/pages/Components/C/Chatbot'
+import HeroSection from '@/pages/Components/H/HeroSection'
+import BreadCrumbs from '@/pages/Components/B/BreadCrumbs'
+import Layout from '../Components/L/Layout'
+import Head from 'next/head'
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Contact</title>
+      </Head>
+      {/* <BreadCrumbs  dynamicText={'Contact Us'} /> */}
+      <ContactUs />
+      <Chatbot />
+    </Layout>
+  )
+}
+
+export default Contact
